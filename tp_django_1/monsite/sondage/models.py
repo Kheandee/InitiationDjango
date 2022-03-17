f9697e2efb38307bb76735e6a8ef16a3c1403d39
Kheandee <94051174+Kheandee@users.noreply.github.com>
2022-03-17 11:45:28 +0100

    Création d'une première question

--- tp_django_1/monsite/sondage/models.py
+++ tp_django_1/monsite/sondage/models.py
@@ -8,8 +8,12 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Date de publication')
 
+    def __str__(self):
+        return self.question_text
+
 class Choice(models.Model):
     #vote = Champ int integer
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+

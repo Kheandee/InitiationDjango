4477eddbd3a9f9204681caf9105d241c1ea1db6a
Kheandee <94051174+Kheandee@users.noreply.github.com>
2022-03-17 14:16:18 +0100

    Page d'accueil du sondage

--- tp_django_1/monsite/sondage/views.py
+++ tp_django_1/monsite/sondage/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from sondage.models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Bienvenue à l'index de notre sondage")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<br>'.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("Voici la question numéro %s." % question_id)
+
+def results(request, question_id):
+    response = "Voici les votes de la question numéro %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vous votez pour la question numéro %s." % question_id)

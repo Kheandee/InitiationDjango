fb57715379afca118878f4078cc6c6790d1c5878
Kheandee <94051174+Kheandee@users.noreply.github.com>
2022-03-17 13:51:06 +0100

    Site administration

--- tp_django_1/monsite/sondage/models.py
+++ tp_django_1/monsite/sondage/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Création de models.
 # Les attributs sont des models.
@@ -8,12 +10,19 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Date de publication')
 
+    #Méthode pour print Question.objects.all()
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     #vote = Champ int integer
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
